fd483af7170587b658ddaf1c792b5698507fc276
Himanshu Joshi <hihi.joshi@gmail.com>
2020-06-10 11:09:01 +1000

    Removing page size since this is now parameterised

--- lovd_downloader/common.py
+++ lovd_downloader/common.py
@@ -6,5 +6,3 @@ lovd_headers = {'Connection':'keep-alive',
                 'Upgrade-Insecure-Requests':'1',
                 'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36',
                 'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'}
-
-page_size = 10
